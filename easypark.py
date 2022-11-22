b3d6a4ff485493aee043ce27a6a4d87bea0979b3
Andrew Spurrier <aspurrie@redhat.com>
2022-11-22 16:54:46 +1100

    removed all time references.

--- easypark.py
+++ easypark.py
@@ -8,8 +8,6 @@ def extract_pdf(filePath):
     receipts = glob(filePath+"Parking_*.pdf")
     # Date pattern
     datePattern = r'\d{4}\.\d{2}\.\d{2}'
-    # Time pattern 
-    timePattern = r'\d{2}\:\d{2}'
     # Price pattern
     pricePattern = r'\d{1,2}\.\d{2} AUD'
     # Iterate over the receipts list 
@@ -22,11 +20,11 @@ def extract_pdf(filePath):
         text = page.extract_text()
         # Extract all dates from the pdf
         extractDate = findall(datePattern, text)
-        # Extract time 
+        # Extract price 
         extractPrice = findall(pricePattern, text)
         # Get date and replace the '.' in the date with no spaces
         date = extractDate[1].replace('.','')
-        # Get start time / end time and replace ':' with no space
+        # Clean up the price and leave it as cents.
         price = extractPrice[0].replace('.','').replace(' AUD','')
         # Rename the file as Parking_date_startTime_endTime.pdf
         rename(receipt, filePath + 'Parking_' + date + '_' + price +'.pdf')
